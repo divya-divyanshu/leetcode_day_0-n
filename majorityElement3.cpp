bf9d203f6df7f6119795ff9c328f76cdc8d5c4d3
Divya Kumar Divyanshu <divyakr.divyanshu@gmail.com>
2024-07-19 00:39:41 +0530

    Leetcode 229. Majority Element II, Used Hashing Technique

---
+++ majorityElement3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> ans;
+        map<int, int> mpp;
+
+        for (int i = 0; i < n; i++){
+            mpp[nums[i]]++;
+        }
+
+        for (auto it : mpp){
+            if ( it.second > (n/3)){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+        
+    }
+};
