2307e3636bef019981d7d98b1a2e9cb31618d329
Divya Kumar Divyanshu <divyakr.divyanshu@gmail.com>
2024-11-10 19:56:22 +0530

    Leetcode - 1614. Maximum Nesting Depth of the Parentheses

---
+++ maxNestingDepth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+
+        int cnt = 0;
+        int maxi  =0;
+        for (int i = 0; i < s.length();i++){
+            if (s[i] == '(') cnt++;
+            else if (s[i] == ')') cnt--;
+
+            maxi = max (cnt, maxi);
+        }
+
+        return maxi;
+        
+    }
+};
