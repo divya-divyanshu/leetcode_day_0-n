7f24910d44261f2186577f4034a0b77c314116a1
Divya Kumar Divyanshu <divyakr.divyanshu@gmail.com>
2024-11-16 07:34:10 +0530

    GFG - Largest Element in Array

---
+++ maxElement.cpp
@@ -0,0 +1,11 @@
+lass Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int maxi = 0;
+        for (int i = 0; i <arr.size();i++){
+            maxi = max(maxi, arr[i]);
+        }
+        
+        return maxi;
+    }
+};
