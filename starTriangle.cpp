1c992936588b16039a7cb71e9a30bf97ed484ea7
Divya Kumar Divyanshu <divyakr.divyanshu@gmail.com>
2025-03-02 10:26:34 +0530

    code360 - Star Triangle

---
+++ starTriangle.cpp
@@ -0,0 +1,19 @@
+void nStarTriangle(int n) {
+    for (int i = 1; i <=n; i++){
+
+        for (int k = 1; k <= n-i; k++){
+            cout<<" ";
+        }
+        // star
+        for (int star = 1; star <= i*2 -1; star++){
+            cout<<"*";
+        }
+        for (int k = 1; k <= n-i; k++){
+            cout<<" ";
+        }
+
+        cout<<endl;
+
+
+    }
+}
