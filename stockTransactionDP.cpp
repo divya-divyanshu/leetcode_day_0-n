36eae94cc9d6a606d6766b575dfc20084a2d660d
Divya Kumar Divyanshu <divyakr.divyanshu@gmail.com>
2025-06-19 11:52:36 +0530

    Leetcode - 714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ stockTransactionDP.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int fn(int fee, int i, bool flag, vector<int>& prices, vector<vector<int>>&dp){
+        int n = prices.size();
+        if(i >= n) return 0;
+
+        int profit = 0;
+
+        if(dp[i][flag] != -1) return dp[i][flag];
+
+        if(flag == true){ // TO BUY
+            int buy = - prices[i] + fn(fee, i+1, false, prices, dp);
+            int notBuy = fn(fee, i+1, true, prices, dp);
+            profit = max(buy, notBuy);
+        }
+        else{ // TO SELL
+            int sell = -fee + prices[i] + fn(fee, i+1, true, prices, dp);
+            int notSell = fn(fee, i+1, false, prices, dp);
+            profit = max(sell, notSell);
+        }
+        return dp[i][flag] = profit;
+
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>>dp(n, vector<int>(2, -1));
+        return fn(fee, 0, true, prices, dp);
+    }
+};
