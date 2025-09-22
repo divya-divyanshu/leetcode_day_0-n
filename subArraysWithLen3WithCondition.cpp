63d1d465bf72c7542ead1bcd661a530642bb445b
Divya <divya-divyanshu>
2025-09-23 02:16:57 +0530

    Leetcode - 3392. Count Subarrays of Length Three With a Condition

---
+++ subArraysWithLen3WithCondition.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int cnt = 0;
+        for(int i  = 0; i < nums.size()-2; i++){
+            if(nums[i+1]%2 ==0 && nums[i]+nums[i+2] == nums[i+1]/2) cnt++;
+        }
+        return cnt;
+    }
+};
