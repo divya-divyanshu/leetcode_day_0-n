f4439e5161ed7f853ab5b583d7d7784ff3b79d73
Divya <divya-divyanshu>
2025-10-31 23:09:48 +0530

    Leetcode - 3289. The Two Sneaky Numbers of Digitville

---
+++ sneakyNumbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int, int>mpp;
+        vector<int>ans;
+        for(auto it:nums){
+            mpp[it]++;
+            if(mpp[it] == 2) ans.push_back(it);
+                    
+        
+        
+        }
+        return ans;
+    }
+};
